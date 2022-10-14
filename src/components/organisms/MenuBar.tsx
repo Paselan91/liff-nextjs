9fd86e34b9d29b64f4a943f77c6fa5675c300553
Paselan91 <kg03272016@gmail.com>
2022-10-14 23:04:40 +0900

    add login, signup page

--- src/components/organisms/MenuBar.tsx
+++ src/components/organisms/MenuBar.tsx
@@ -18,7 +18,62 @@ import {
 } from '@chakra-ui/react'
 import { FC, memo } from 'react'
 
-const MenuBar: FC = () => {
+interface Props {
+  navItems: NavItem[]
+}
+interface NavItem {
+  label: string
+  subLabel?: string
+  children?: NavItem[]
+  href?: string
+}
+
+const navItems: NavItem[] = [
+  {
+    label: 'Inspiration',
+    children: [
+      {
+        label: 'Explore Design Work',
+        subLabel: 'Trending Design to inspire you',
+        href: '#',
+      },
+      {
+        label: 'New & Noteworthy',
+        subLabel: 'Up-and-coming Designers',
+        href: '#',
+      },
+    ],
+  },
+  {
+    label: 'Find Work',
+    children: [
+      {
+        label: 'Job Board',
+        subLabel: 'Find your dream design job',
+        href: '#',
+      },
+      {
+        label: 'Freelance Projects',
+        subLabel: 'An exclusive list for contract work',
+        href: '#',
+      },
+    ],
+  },
+  {
+    label: 'Learn Design',
+    href: '#',
+  },
+  {
+    label: 'Hire Designers',
+    href: '#',
+  },
+]
+
+
+const MenuBar: FC<Props> = ({navItems}) => {
+  const loginUrl = '/login'
+  const signUpUrl = '/signup'
+  const topUrl = '/papers'
   const { isOpen, onToggle } = useDisclosure()
 
   return (
@@ -47,21 +102,24 @@ const MenuBar: FC = () => {
           />
         </Flex>
         <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
-          <Text
+          <Button 
+            as={'a'}
+            variant={'link'} 
+            href={topUrl}
             textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
             fontFamily={'heading'}
             color={useColorModeValue('gray.800', 'white')}
-          >
+            style={{ textDecoration: 'none' }}
+            >
             Logo
-          </Text>
-
+          </Button>
           <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
             <DesktopNav />
           </Flex>
         </Flex>
 
         <Stack flex={{ base: 1, md: 0 }} justify={'flex-end'} direction={'row'} spacing={6}>
-          <Button as={'a'} fontSize={'sm'} fontWeight={400} variant={'link'} href={'#'}>
+          <Button as={'a'} fontSize={'sm'} fontWeight={400} variant={'link'} href={loginUrl}>
             Sign In
           </Button>
           <Button
@@ -70,7 +128,8 @@ const MenuBar: FC = () => {
             fontWeight={600}
             color={'white'}
             bg={'pink.400'}
-            href={'#'}
+            as={'a'}
+            href={signUpUrl}
             _hover={{
               bg: 'pink.300',
             }}
@@ -94,7 +153,7 @@ const DesktopNav = () => {
 
   return (
     <Stack direction={'row'} spacing={4}>
-      {NAV_ITEMS.map((navItem) => (
+      {navItems.map((navItem) => (
         <Box key={navItem.label}>
           <Popover trigger={'hover'} placement={'bottom-start'}>
             <PopoverTrigger>
@@ -172,7 +231,7 @@ const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
 const MobileNav = () => {
   return (
     <Stack bg={useColorModeValue('white', 'gray.800')} p={4} display={{ md: 'none' }}>
-      {NAV_ITEMS.map((navItem) => (
+      {navItems.map((navItem) => (
         <MobileNavItem key={navItem.label} {...navItem} />
       ))}
     </Stack>
@@ -228,53 +287,5 @@ const MobileNavItem = ({ label, children, href }: NavItem) => {
     </Stack>
   )
 }
-
-interface NavItem {
-  label: string
-  subLabel?: string
-  children?: Array<NavItem>
-  href?: string
-}
-
-const NAV_ITEMS: Array<NavItem> = [
-  {
-    label: 'Inspiration',
-    children: [
-      {
-        label: 'Explore Design Work',
-        subLabel: 'Trending Design to inspire you',
-        href: '#',
-      },
-      {
-        label: 'New & Noteworthy',
-        subLabel: 'Up-and-coming Designers',
-        href: '#',
-      },
-    ],
-  },
-  {
-    label: 'Find Work',
-    children: [
-      {
-        label: 'Job Board',
-        subLabel: 'Find your dream design job',
-        href: '#',
-      },
-      {
-        label: 'Freelance Projects',
-        subLabel: 'An exclusive list for contract work',
-        href: '#',
-      },
-    ],
-  },
-  {
-    label: 'Learn Design',
-    href: '#',
-  },
-  {
-    label: 'Hire Designers',
-    href: '#',
-  },
-]
 
 export default memo(MenuBar)
