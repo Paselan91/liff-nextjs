69a4f458f2b87408c663ff296a3764df64e57ff8
Paselan91 <kg03272016@gmail.com>
2022-11-24 08:50:03 +0900

    不要な記述,ページ削除

--- src/graphql/apllo-client.ts
+++ src/graphql/apllo-client.ts
@@ -1,16 +1,11 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
-console.log('apllo-client')
-
 // TODO: 要調査 Localでdocker-composeを使用している理由から、SSGとCSRでエンドポイントを分ける必要がありそう
 // docker-composeの設定を変えれば不要？ backendを host networkにする?
 let beUrl = `${process.env.NEXT_PUBLIC_TEST_BACKEND_URL}`
 if (typeof window !== 'undefined') {
   beUrl = `${process.env.NEXT_PUBLIC_CLIENT_BACKEND_URL}`
 }
-
-console.log('beUrl')
-console.log(beUrl)
 
 const apolloClient = new ApolloClient({
   uri: beUrl,
