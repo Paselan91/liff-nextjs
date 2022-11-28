30ab88d50edfbbc5de4351acc6d63f46eda0ab47
Paselan91 <kg03272016@gmail.com>
2022-11-28 20:36:11 +0900

    BEと繋ぎ込み

--- src/graphql/queries/post/mutation.ts
+++ src/graphql/queries/post/mutation.ts
@@ -1,5 +1,17 @@
 import { gql } from '@apollo/client'
 
+export const UPDATE_POST = gql`
+  mutation updatePost($input: UpdatePostInput!) {
+    updatePost(input: $input) {
+      id
+      title
+      body
+      image_url
+      is_public
+    }
+  }
+`
+
 export const DELETE_POST = gql`
   mutation deletePost($post_id: Int!) {
     deletePost(post_id: $post_id)
--- src/pages/posts/[id]/edit.tsx
+++ src/pages/posts/[id]/edit.tsx
@@ -1,3 +1,4 @@
+import { useMutation } from '@apollo/client'
 import {
   Container,
   Stack,
@@ -7,22 +8,61 @@ import {
   InputGroup,
   Radio,
   RadioGroup,
+  Button,
+  useToast,
 } from '@chakra-ui/react'
 import type { NextPage, GetServerSideProps } from 'next'
-import { memo, useState } from 'react'
+import { memo, useState, useEffect, ChangeEvent } from 'react'
 
 import apolloClient from '@/graphql/apllo-client'
+import { UPDATE_POST } from '@/graphql/queries/post/mutation'
 import { FETCH_POST_BY_ID } from '@/graphql/queries/post/query'
-import { Post } from '@/types/generated/graphql'
+import { Post, UpdatePostInput } from '@/types/generated/graphql'
 
 interface Props {
   post: Post
 }
 
 const PostEdit: NextPage<Props> = ({ post }) => {
-  const [isPublic, setIsPublic] = useState('1')
+  const [title, setTitle] = useState(post.title)
+  const [body, setBody] = useState(post.body)
+  const [isPublic, setIsPublic] = useState(post.is_public ? '1' : '0')
 
-  post.is_public ? setIsPublic('1') : setIsPublic('0')
+  const toast = useToast()
+
+  const [updatePost, { data }] = useMutation(UPDATE_POST)
+
+  useEffect(() => {
+    if (data) {
+      toast({
+        title: 'Successed, update post.',
+        status: 'success',
+        duration: 9000,
+        isClosable: true,
+      })
+    }
+  }, [data])
+
+  const onChangeTitle = (e: ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)
+  const onChangeBoby = (e: ChangeEvent<HTMLInputElement>) => setBody(e.target.value)
+
+  const update = async () => {
+    const input: UpdatePostInput = {
+      post_id: Number(post.id),
+      title: title,
+      body: body,
+      is_public: isPublic == '1' ? true : false,
+    }
+    try {
+      await updatePost({
+        variables: {
+          input: input,
+        },
+      })
+    } catch (err) {
+      return
+    }
+  }
 
   return (
     <>
@@ -31,25 +71,43 @@ const PostEdit: NextPage<Props> = ({ post }) => {
           <FormControl isRequired>
             <FormLabel>Title</FormLabel>
             <InputGroup>
-              <Input type='title' name='title' placeholder={post.title} />
+              <Input type='title' name='title' value={title} onChange={onChangeTitle} />
             </InputGroup>
           </FormControl>
           <FormControl isRequired>
             <FormLabel>Body</FormLabel>
             <InputGroup>
-              <Input type='body' name='body' placeholder={post.body} />
+              <Input type='body' name='body' value={body} onChange={onChangeBoby} />
             </InputGroup>
           </FormControl>
           <FormControl isRequired>
             <FormLabel>Is Public</FormLabel>
             <RadioGroup onChange={setIsPublic} value={isPublic}>
-              <Stack direction='row'>
+              <Stack direction='column'>
                 <Radio value='1'>Public</Radio>
                 <Radio value='0'>Not Public</Radio>
               </Stack>
             </RadioGroup>
           </FormControl>
         </Stack>
+        <Button
+          onClick={update}
+          my='5'
+          mx='auto'
+          size='lg'
+          fontSize={'sm'}
+          rounded={'full'}
+          color={'white'}
+          bg={'blue.300'}
+          _hover={{
+            bg: 'blue.500',
+          }}
+          _focus={{
+            bg: 'blue.500',
+          }}
+        >
+          Update
+        </Button>
       </Container>
     </>
   )
